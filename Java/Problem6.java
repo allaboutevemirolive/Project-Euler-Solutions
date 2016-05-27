381cb8a088235713240b3d63d7005f18063afc9f
kkmonlee <kkmonlee@gmail.com>
2016-05-27 19:34:44 +0100

    Added a few solutions
    I'm yet to add these to the tabled in the Markdown file

---
+++ Java/Problem6.java
@@ -0,0 +1,18 @@
+public class Problem6 {
+
+  final int CONST = 100;
+
+  Problem6() {
+    int sum = 0;
+    int sum2 = 0;
+    int index = 0;
+
+    do {
+      sum += i;
+      sum2 += i * i;
+
+      index++;
+    } while (index <= CONST)
+  }
+
+}
