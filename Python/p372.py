ec5b45222eab466b84e2f0b1ab0f03f9336d0ade
kkmonlee <kkmonlee@gmail.com>
2017-01-19 06:28:29 +0000

    Lattice counting algorithm for right triangles

---
+++ Python/p372.py
@@ -0,0 +1,22 @@
+from decimal import *
+
+def getNumberOfPoints(a, b, c):
+    m = c / a
+    if a == b:
+        return (m * (m - 1)) / 2
+    k, h = (a - 1) / b, (c - a * m) / b
+
+    return getNumberOfPoints(b, a - b * k, c - b * (k * m + h)) + k * (m * (m - 1)) / 2 + m * h
+
+def getLatticePoints(k, M, N):
+    s, q = Decimal(k).sqrt(), 10**30
+    p = int(s * q)
+    if N * q - M * p <= 0: return 0
+
+    return getNumberOfPoints(p, q, N * q - M * p) + (N * q) / p - M
+
+def getRay(k, M, N):
+    return getLatticePoints(2 * k + 1, M, N)
+
+M, N = 2*10**6, 10**9
+print sum([getRay(k, M, N) for k in xrange(0, (N * N) / (M * M) + 1)])
