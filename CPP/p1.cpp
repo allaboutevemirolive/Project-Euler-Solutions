70978cf1833e2eb8370f811e6471185eaad211c9
kkmonlee <kkmonlee@gmail.com>
2017-01-19 06:45:44 +0000

    Added 1-4 C++ solutions

---
+++ CPP/p1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int res = 0;
+	for (int i = 1; i < 1000; i++) {
+		if ((i % 3 == 0) || (i % 5 == 0)) {
+			res += i;
+		}
+	}
+	cout << res << endl;
+
+	int wait = 0;
+	cin >> wait;
+
+	return 0;
+}
