2f8c6fd65707721f6ccab3bfcd8372f48259749b
kkmonlee <kkmonlee@gmail.com>
2017-07-08 12:36:15 +0100

    Harmonic analysis, inverse sum

---
+++ CCPP/p441.cpp
@@ -0,0 +1,82 @@
+#include <cstdio>
+
+const int N = 10000000;
+double H[N+1];
+int minp[N+1];
+
+void primes(int n, int& k, int* pr) {
+    k = 0;
+    while (n > 1) {
+        int p = minp[n];
+        pr[k++] = p;
+        while (n % p == 0) n /= p;
+    }
+}
+
+double invsum(int k, int* pr, int n) {
+    double res = 0;
+    for (int mask = 0; mask < (1 << k); mask++) {
+        int prod = 1;
+        int sign = 1;
+        for (int i = 0; i < k; i++) {
+            if (mask & (1 << i)) {
+                sign = -sign;
+                prod *= pr[i];
+            }
+        }
+        res += sign * H[n / prod] / prod;
+    }
+    return res;
+}
+
+double count(int k, int* pr, int n) {
+    int res = 0;
+    for (int mask = 0; mask < (1 << k); mask++) {
+        int prod = 1;
+        int sign = 1;
+        for (int i = 0; i < k; i++) {
+            if (mask & (1 << i)) {
+                sign = -sign;
+                prod *= pr[i];
+            }
+        }
+        res += sign * (n / prod);
+    }
+    return res;
+}
+
+int main() {
+    // sieve
+    for (int i = 2; i <= N; i++) if (!minp[i])
+        for (int j = i; j <= N; j += i) if (!minp[j])
+            minp[j] = i;
+    
+    // harmonic nums
+    H[0] = 0.;
+    for (int n = 1; n <= N; n++) {
+        H[n] = H[n - 1] + 1. / n;
+    }
+
+    double res = 0;
+    for (int q = 2; q <= N; q++) {
+        int k;
+        int pr[111];
+        primes(q, k, pr);
+        int n = q - 1 < N - q ? q - 1 : N - q;
+        // calc this only once
+        double min_sum = invsum(k, pr, n);
+        if (q - 1 > n) {
+            double sum = invsum(k, pr, q - 1) - min_sum;
+            res += (N - q + 1.) / q * sum;
+        }
+
+        if (q < N) {
+            double sum = min_sum + count(k, pr, n);
+            res += sum / q;
+        }
+    }
+
+    printf("%.4lf\n", res);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -165,6 +165,7 @@ I haven't done every solution in every language so here's a table to make things
 | Problem 438 | | | |[Solution](https://github.com/kkmonlee/Project-Euler-Solutions/blob/master/Python/p438.py)| | | | |
 | Problem 439 | | | | | |[Solution](https://github.com/kkmonlee/Project-Euler-Solutions/blob/master/CCPP/p439.cpp)| | |
 | Problem 440 | | | | | |[Solution](https://github.com/kkmonlee/Project-Euler-Solutions/blob/master/CCPP/p440.cpp)| | |
+| Problem 441 | | | | | |[Solution](https://github.com/kkmonlee/Project-Euler-Solutions/blob/master/CCPP/p441.cpp)| | |
 | Problem 447 | | | | | |[Solution](https://github.com/kkmonlee/Project-Euler-Solutions/blob/master/CCPP/p447.cpp)| |
 | Problem 478 | | | | | |[Solution](https://github.com/kkmonlee/Project-Euler-Solutions/blob/master/CCPP/p478.cpp)| | |
 | Problem 495 |    |   |   |    |   |[Solution](https://github.com/kkmonlee/Project-Euler-Solutions/blob/master/CCPP/p495.cpp)|  |  |
