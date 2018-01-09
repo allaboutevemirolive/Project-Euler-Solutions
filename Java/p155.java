169ad22ff0ec3eca6a8d279fc72505bfabdf1b3e
kkmonlee <kkmonlee@gmail.com>
2018-01-09 17:59:06 +0000

    Cleaned directories

--- Java/p155.java
+++ Java/p155.java
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-public class Main {
-  public static void main(String[] args) {
-    Q1 problem1 = new Q1();   // Creating an instance so I can compile whatever
-                              // method I want
-
-    // c
-    
-  }
-=======
 import java.math.BigInteger;
 import java.util.HashSet;
 import java.util.Set;
